17dde4d1c639a2fe924cb89ba2d6c77b1dc5dbc2
Farhan452002 <farhan.ahmed1@mail.citytech.cuny.edu>
2025-02-06 01:28:00 -0500

    Added EditQuestions to navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import PlayGame from "./pages/PlayGame";
 import AddQuestion from "./pages/AddQuestion";
 import ViewQuestions from "./pages/ViewQuestions";
+import EditQuestions from "./pages/EditQuestions";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
                     <Link to="/">Play Game</Link>
                     <Link to="/add-question">Add Question</Link>
                     <Link to="/view-questions">View Questions</Link>
+                    <Link to="/edit-questions">Edit Questions</Link>
                 </nav>
 
                 <h1>Jeopardy Board Game</h1>
@@ -20,6 +22,7 @@ function App() {
                     <Route path="/" element={<PlayGame />} />
                     <Route path="/add-question" element={<AddQuestion />} />
                     <Route path="/view-questions" element={<ViewQuestions />} />
+                    <Route path="/edit-questions" element={<EditQuestions />} />
                 </Routes>
             </div>
         </Router>
